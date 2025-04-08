ab93deaa5cdaf21e7ddfecaed72d4bb0a4fe6a38
gustavo <gustavo.isaac@gmail.com>
2025-04-08 14:24:36 +0100

    Initial commit

---
+++ src/main/kotlin/com/artemissoftware/calliopenotes/CalliopenotesApplication.kt
@@ -0,0 +1,11 @@
+package com.artemissoftware.calliopenotes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalliopenotesApplication
+
+fun main(args: Array<String>) {
+	runApplication<CalliopenotesApplication>(*args)
+}
