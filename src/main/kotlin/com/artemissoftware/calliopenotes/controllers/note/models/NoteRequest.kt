eb88a9a6455735aa4e25146bab61f2f40dfe4b51
gustavo <gustavo.isaac@gmail.com>
2025-05-06 15:51:17 +0100

    Validation of fields

--- src/main/kotlin/com/artemissoftware/calliopenotes/controllers/note/models/NoteRequest.kt
+++ src/main/kotlin/com/artemissoftware/calliopenotes/controllers/note/models/NoteRequest.kt
@@ -1,8 +1,10 @@
 package com.artemissoftware.calliopenotes.controllers.note.models
 
+import jakarta.validation.constraints.NotBlank
+
 data class NoteRequest(
     val id: String?,
-    //@field:NotBlank(message = "Title can't be blank.")
+    @field:NotBlank(message = "Title can't be blank.")
     val title: String,
     val content: String,
     val color: Long,
