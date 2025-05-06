b320e64690b558c88f5482934facab31f4c83ee1
gustavo <gustavo.isaac@gmail.com>
2025-05-06 15:16:28 +0100

    AuthRequest

---
+++ src/main/kotlin/com/artemissoftware/calliopenotes/controllers/auth/models/AuthRequest.kt
@@ -0,0 +1,13 @@
+package com.artemissoftware.calliopenotes.controllers.auth.models
+
+data class AuthRequest(
+    //@field:Email(message = "Invalid email format.")
+    val email: String,
+    /*
+    @field:Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{9,}\$",
+        message = "Password must be at least 9 characters long and contain at least one digit, uppercase and lowercase character."
+    )
+    */
+    val password: String
+)
