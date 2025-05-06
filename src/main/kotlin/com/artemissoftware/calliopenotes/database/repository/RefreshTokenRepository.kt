5f369bb8c1f46d9df3de993ac91398530f66b20d
gustavo <gustavo.isaac@gmail.com>
2025-05-06 15:01:44 +0100

    RefreshTokenRepository

---
+++ src/main/kotlin/com/artemissoftware/calliopenotes/database/repository/RefreshTokenRepository.kt
@@ -0,0 +1,10 @@
+package com.artemissoftware.calliopenotes.database.repository
+
+import com.artemissoftware.calliopenotes.database.model.RefreshToken
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface RefreshTokenRepository: MongoRepository<RefreshToken, ObjectId> {
+    fun findByUserIdAndHashedToken(userId: ObjectId, hashedToken: String): RefreshToken?
+    fun deleteByUserIdAndHashedToken(userId: ObjectId, hashedToken: String)
+}
