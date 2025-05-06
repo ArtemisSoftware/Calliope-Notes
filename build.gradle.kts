bc67cc3ba279e8276b44c9de21294ba6909be098
gustavo <gustavo.isaac@gmail.com>
2025-05-06 15:35:29 +0100

    Validation dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
